9a8e08f961904eff5df3399ad92fcf06021b277e
HiroGori <hiro630705@gmail.com>
2021-03-23 15:12:03 +0900

    Modified web.php and created TweetsController.php.

---
+++ app/Http/Controllers/TweetsController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Tweet;
+
+class TweetsController extends Controller
+{
+    public function index()
+    {
+        $tweets = Tweet::orderBy('created_at', desc')->get();
+
+        return view('tweets.index', ['tweets' => $posts]);
+}
