420739b87f68b0059cfbe9d77af3fba0e6aee1fa
HiroGori <hiro630705@gmail.com>
2021-03-26 14:52:17 +0900

    Created show.blade.php for view.

--- resources/views/tweets/show.blade.php
+++ resources/views/tweets/show.blade.php
@@ -1,5 +1,34 @@
 @extends('layout')
 
 @section('content')
+    <<div class="container mt-4">
+        <div class="border p-4">
+            <h1 class="h5 mb-4">
+                {{ $tweet->user->user_name }}
+            </h1>
 
+            <p class="mb-5">
+                {!! nl2br(e($tweet->body)) !!}
+            </p>
+
+            <section>
+                <h2 class="h5 mb-4">
+                    コメント
+                </h2>
+
+                @forelse($tweet->comments as $comment)
+                    <div class="border-top p-4">
+                        <time class="text-secondary">
+                            {{ $comment->created_at->format('Y.m.d H:i') }}
+                        </time>
+                        <p class="mt-2">
+                            {!! nl2br(e($comment->body)) !!}
+                        </p>
+                    </div>
+                @empty
+                    <p>コメントはまだありません。</p>
+                @endforelse
+            </section>
+        </div>
+    </div>
 @endsection
