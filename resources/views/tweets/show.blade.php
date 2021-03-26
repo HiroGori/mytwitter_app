5c10edca5387b8102c05853571a81f9123350178
HiroGori <hiro630705@gmail.com>
2021-03-26 16:02:07 +0900

    Added link to show page on index page.

--- resources/views/tweets/show.blade.php
+++ resources/views/tweets/show.blade.php
@@ -1,34 +1,38 @@
 @extends('layout')
 
 @section('content')
-    <<div class="container mt-4">
-        <div class="border p-4">
-            <h1 class="h5 mb-4">
+    <div class="container mt-4">
+        <div class="card mb-4">
+            <div class="card-header">
                 {{ $tweet->user->user_name }}
-            </h1>
-
-            <p class="mb-5">
-                {!! nl2br(e($tweet->body)) !!}
-            </p>
-
-            <section>
-                <h2 class="h5 mb-4">
-                    コメント
-                </h2>
-
-                @forelse($tweet->comments as $comment)
-                    <div class="border-top p-4">
-                        <time class="text-secondary">
-                            {{ $comment->created_at->format('Y.m.d H:i') }}
-                        </time>
-                        <p class="mt-2">
+            </div>
+            <div class="card-body">
+                <p class="card-text">
+                    {!! nl2br(e($tweet->body)) !!}
+                </p>
+            </div>
+            <div class="card-footer">
+                <span class="mr-2">
+                    投稿日時 {{ $tweet->created_at->format('Y.m.d') }}
+                </span>
+            </div>
+        </div>
+        <section>
+            <h2 class="h5 mb-4">
+                コメント
+            </h2>
+            @forelse($tweet->comments as $comment)
+                <div class="border-top p-4">
+                    <time class="text-secondary">
+                        {{ $comment->created_at->format('Y.m.d H:i') }}
+                    </time>
+                    <p class="mt-2">
                             {!! nl2br(e($comment->body)) !!}
-                        </p>
-                    </div>
+                    </p>
+                </div>
                 @empty
                     <p>コメントはまだありません。</p>
-                @endforelse
-            </section>
-        </div>
+            @endforelse
+        </section>
     </div>
 @endsection
